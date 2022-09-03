d42d6b88654ff650867ec8b254d1d2ba62ad7b75
thormighti <peterabeshi4@gmail.com>
2022-09-03 18:27:28 +0100

    created a json objects

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,17 @@
 use std::error::Error;
 use std::io;
 use std::process;
+use std::collections::HashMap;
 
 use serde::Deserialize;
+use serde::Serialize;
 
 
 
  #[derive(Debug, Deserialize)]
 // #[serde(rename_all = "camelCase")]
  struct Record {
+    //used options incase field is empty, String might be pretty expensive
     A: Option<String>,
     B: Option<String>,
     C: Option<String>,
@@ -17,8 +20,10 @@ use serde::Deserialize;
     F: Option<String>
 }
 
-fn example() -> Result<Vec<Record>, Box<dyn Error>> {
+fn run() -> Result<Vec<Record>, Box<dyn Error>> {
     let mut rdr = csv::Reader::from_reader(io::stdin());
+
+    //store vector of the records
         let mut records = vec![];
     
     for result in rdr.deserialize() {
@@ -26,15 +31,11 @@ fn example() -> Result<Vec<Record>, Box<dyn Error>> {
         // deserialization.
         let record: Option<String> = result?;
         // println!("{:?}", record);
-
-    
-
     
         if let Some(data) = record{
             let column = parse_column(data);
 
-            records.push(column);
-            
+            records.push(column);   
 
         }
     }
@@ -44,14 +45,14 @@ fn example() -> Result<Vec<Record>, Box<dyn Error>> {
 }
 
 fn main() {
-    if let Ok(err) = example() {
+    if let Ok(err) = run() {
         
 
     test_one(err);
 
     }
     
-      if let  Err(err) = example(){
+      if let  Err(err) = run(){
              println!("error running example: {}", err);
          process::exit(1);
 
@@ -65,16 +66,19 @@ fn parse_column(input: String) -> Record {
 
     let record_fields: Vec<_> = input.split(";").collect();
 
+    //rows should contain atleast 5 fields
+
      if record_fields.len() < 5{
          println!("Unexpected number of columns");
      };
 
+    // parsing the c and d column to integers
     let d :i32= record_fields[3].trim().parse().unwrap_or(0);
     let e :i32= record_fields[4].trim().parse().unwrap_or(0);
 
 
 
-
+//Realized some rows had 6 fields and some had 5, had to address them here
 if record_fields.len() == 5 {
   Record{
     A: Some(record_fields[0].to_string()),
@@ -100,18 +104,56 @@ else {
 
 }
 
-//Checks for sum of c and d if > 100
+ #[derive(Debug, Deserialize,Serialize)]
+struct RecordJsonInner{
+    line_number : u32,
+    types: String,
+    concat_ab : String,
+    sum_cd : i32
+
+}
+
+ #[derive(Debug, Deserialize,Serialize)]
+ struct Record_Json_Erro{
+    line_number : u32,
+    types: String,
+    error_messages: String
+
+ }
+
+
+
+
+type RecordJson = HashMap<String,RecordJsonInner>;
+
+//Checks for sum of c and d if > 100 in test one
 fn test_one(records:Vec<Record>){
 
-    for rows in &records{
+    for (index,rows) in records.into_iter().enumerate(){
         if let (Some(e), Some(d)) = (&rows.D, &rows.E){
-            if e + d  > 100{
+            let sum_cd = e +d;
+            if sum_cd  > 100{
                 
                 if let (Some(b) , Some(c)) = (&rows.B, &rows.C){
-                    println!("{}{}", b,c);
+                    let json_obj_inner_struct = RecordJsonInner{
+                        line_number: index as u32 +1,
+                        types: "Ok".to_string(),
+                        concat_ab: format!("{} {}", c, d),
+                        sum_cd
+                    };
+                    let convert_to_json = serde_json::to_value(json_obj_inner_struct);
+
+                    println!("{:?}", convert_to_json);
+                    // println!("{}  {}  {}",index+1, b,c);
                 }
             }
 
         }
     }
 }
+/* 
+
+Rows that can be processed correctly with C + D > 100: { "lineNumber":
+ <FILE_LINE_NUMBER>, "type": "ok", "concatAB": "<PREVIOUS_AB_CONCAT>", "sumCD": <PREVIOUS_CD_SUM> }
+
+  */
